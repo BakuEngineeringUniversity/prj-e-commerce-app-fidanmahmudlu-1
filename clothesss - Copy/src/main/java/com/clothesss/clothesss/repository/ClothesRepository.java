9d5b60eff97244636030b7d5edd9be7877959b67
Aykhan Alakbarov <126717367+fidanmahmudlu@users.noreply.github.com>
2024-01-18 08:34:12 +0400

    adding security

--- clothesss - Copy/src/main/java/com/clothesss/clothesss/repository/ClothesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.clothesss.clothesss.repository;
-
-import com.clothesss.clothesss.Entity.ClothesItem;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ClothesRepository extends MongoRepository<ClothesItem, String> {
-}
