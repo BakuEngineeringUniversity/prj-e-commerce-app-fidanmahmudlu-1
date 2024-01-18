d8ed579cb89bd08b38b0402d3b3f6c710a5aebc4
Aykhan Alakbarov <126717367+fidanmahmudlu@users.noreply.github.com>
2024-01-18 08:37:24 +0400

    fixing error

--- Clothingapp - Copy/Clothingapp/src/main/java/com/Clothingapp/Clothingapp/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Clothingapp.Clothingapp.repository;
-import org.springframework.stereotype.Repository;
-import com.Clothingapp.Clothingapp.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User,Long>{
-
-}
