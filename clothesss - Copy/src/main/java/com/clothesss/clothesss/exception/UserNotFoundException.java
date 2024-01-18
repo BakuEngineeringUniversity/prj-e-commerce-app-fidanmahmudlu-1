17dac3d901c97f53583ff2723a2644d4aa65f38e
Aykhan Alakbarov <126717367+fidanmahmudlu@users.noreply.github.com>
2024-01-18 08:32:49 +0400

    fix error

--- clothesss - Copy/src/main/java/com/clothesss/clothesss/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.clothesss.clothesss.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
